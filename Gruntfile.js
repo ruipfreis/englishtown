b261c381e8e00a42c072d2a4ea6ef261e6355a73
Rui Reis <ruipfreis@gmail.com>
2015-09-10 12:18:25 +0100

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,170 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  //var connect = require('connect');
+  var serveStatic = require('serve-static');
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  // Configurable paths for the application
+  var appConfig = {
+    app: require('./bower.json').appPath || '.',
+    dist: 'dist'
+  };
+
+  grunt.initConfig({
+
+    appConfig: appConfig,
+
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      bower: {
+        files: ['bower.json'],
+        tasks: ['wiredep']
+      },
+      js: {
+        files: ['<%= appConfig.app %>/scripts/{,*/}*.js'],
+        tasks: ['newer:jshint:all'],
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        }
+      },
+      sass: {
+        files: ['<%= appConfig.app %>/styles/{,*/}*.{scss,sass}'],
+        tasks: ['sass:server', 'autoprefixer']
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= appConfig.app %>/{,*/}*.html',
+          '.tmp/styles/{,*/}*.css',
+          '<%= appConfig.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      }
+    },
+
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9000,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost',
+        livereload: 34729
+      },
+      livereload: {
+        options: {
+          open: true,
+          middleware: function (connect) {
+            return [
+              serveStatic('.tmp'),
+              connect().use(
+                '/bower_components',
+                serveStatic('./bower_components')
+              ),
+              serveStatic(appConfig.app)
+            ];
+          }
+        }
+      }
+    },
+
+    // Make sure code styles are up to par and there are no obvious mistakes
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: {
+        src: [
+          'Gruntfile.js',
+          '<%= appConfig.app %>/scripts/{,*/}*.js'
+        ]
+      }
+    },
+
+    // Empties folders to start fresh
+    clean: {
+      server: '.tmp'
+    },
+
+    // Add vendor prefixed styles
+    autoprefixer: {
+      options: {
+        browsers: ['last 1 version']
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/styles/',
+          src: '{,*/}*.css',
+          dest: '.tmp/styles/'
+        }]
+      }
+    },
+
+    // Automatically inject Bower components into the app
+    wiredep: {
+      app: {
+        src: ['<%= appConfig.app %>/index.html'],
+        ignorePath:  /\.\.\//
+      },
+      sass: {
+        src: ['<%= appConfig.app %>/styles/{,*/}*.{scss,sass}'],
+        ignorePath: /(\.\.\/){1,2}bower_components\//
+      }
+    },
+
+    // Grunt-sass
+    sass: {
+      server: {
+        // Takes every file that ends with .scss from the scss
+        // directory and compile them into the css directory.
+        // Also changes the extension from .scss into .css.
+        // Note: file name that begins with _ are ignored automatically
+        files: [{
+          expand: true,
+          cwd: '<%= appConfig.app %>/styles',
+          src: ['*.scss', '*.sass'],
+          dest: '.tmp/styles',
+          ext: '.css'
+        }]
+      },
+      options: {
+        sourceMap: true,
+        outputStyle: 'nested',
+        imagePath: "<%= appConfig.app %>/images",
+      }
+    },
+
+    // Run some tasks in parallel to speed up the build process
+    concurrent: {
+      server: [
+        'sass:server'
+      ],
+      test: [
+        'sass'
+      ]
+    }
+  });
+
+  grunt.registerTask('serve', 'Compile then start a connect web server', function () {
+    grunt.task.run([
+      'clean:server',
+      'wiredep',
+      'concurrent:server',
+      'autoprefixer',
+      'connect:livereload',
+      'watch'
+    ]);
+  });
+};
